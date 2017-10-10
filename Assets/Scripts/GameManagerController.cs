9f98c721a4914a500e809f17ca2fd2a18fb25889
Leonard Andrew Spencer <thrawn369@gmail.com>
2017-10-10 13:25:35 +1030

    Added scenes to build

--- Assets/Scripts/GameManagerController.cs
+++ Assets/Scripts/GameManagerController.cs
@@ -6,7 +6,7 @@ public class GameManagerController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		DontDestroyOnLoad(gameObject);
 	}
 	
 	// Update is called once per frame
