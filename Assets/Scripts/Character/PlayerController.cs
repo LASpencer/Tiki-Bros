f738a9c05c5504eb6add45e3434ac9e2d566573d
Leonard Andrew Spencer <thrawn369@gmail.com>
2017-10-16 10:30:42 +1030

    Added air and ground speeds to playercontroller fields

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -5,7 +5,8 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
+    public float GroundSpeed;
+    public float AirSpeed; // TODO not yet implemented
     public float jumpForce;
     public float gravityScale;
 	public int treasureColleceted;
@@ -36,20 +37,12 @@ public class PlayerController : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update ()
-    { /*
-        playerRB.velocity = new Vector3 (Input.GetAxis("Horizontal") * moveSpeed, playerRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
-        if(Input.GetButtonDown("Jump"))
-        {
-            playerRB.velocity = new Vector3(playerRB.velocity.x, jumpForce, playerRB.velocity.z);
-        } */
-
-        //        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
-
+    { 
         float yStore = moveDirection.y;
         //Debug.Log("vert: " + Input.GetAxis("Vertical") + " horiz: " + Input.GetAxis("Horizontal"));
         moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
         //moveDirection = moveDirection.normalized * moveSpeed;
-        moveDirection = Vector3.ClampMagnitude(moveDirection, 1f) * moveSpeed;
+        moveDirection = Vector3.ClampMagnitude(moveDirection, 1f) * GroundSpeed;
         moveDirection.y = yStore;
 
         if (controller.isGrounded)
---
+++ Assets/Scripts/Character/PlayerState.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class PlayerState  {
+
+    private PlayerController player;
+    PlayerController Player { get { return Player; } }
+
+    public PlayerState(PlayerController player)
+    {
+        this.player = player;
+    }
+
+    abstract public void OnEnter();
+
+    abstract public void Update();
+
+    abstract public void OnExit();
+}
+
+public class IdleState : PlayerState
+{
+    public IdleState(PlayerController player) : base(player)
+    {
+    }
+
+    public override void OnEnter()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void OnExit()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Update()
+    {
+        throw new NotImplementedException();
+    }
+}
