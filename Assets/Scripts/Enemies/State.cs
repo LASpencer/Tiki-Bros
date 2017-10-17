904d3137a1073f791dbbf712092c60ed137ea042
Leonard Andrew Spencer <thrawn369@gmail.com>
2017-10-17 11:43:12 +1030

    Created EnemyAI state machine classes

---
+++ Assets/Scripts/Enemies/State.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "EnemyAI/State")]
+public class State : ScriptableObject {
+
+    public List<Action> actions;
+    public List<Transition> transitions;
+    public Color sceneGizmoColor = Color.gray;
+
+    public void UpdateState(EnemyController controller)
+    {
+        DoActions(controller);
+        CheckTransitions(controller);
+    }
+
+    private void DoActions(EnemyController controller)
+    {
+        foreach(Action action in actions)
+        {
+            action.Act(controller);
+        }
+    }
+
+    private void CheckTransitions(EnemyController controller)
+    {
+        State nextState;
+        foreach(Transition transition in transitions)
+        {
+            if (transition.decision.Decide(controller))
+            {
+                nextState = transition.TrueState;
+            } else
+            {
+                nextState = transition.FalseState;
+            }
+            if(nextState != controller.CurrentState)
+            {
+                controller.ChangeState(nextState);
+                break;
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class Transition
+{
+    public Decision decision;
+    public State TrueState;
+    public State FalseState;
+}
