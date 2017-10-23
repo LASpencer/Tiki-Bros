152ec43fe880c7862d5ce6609ddeafbf76f19f0f
Leonard Andrew Spencer <thrawn369@gmail.com>
2017-10-23 15:05:20 +1030

    Created singleton GameManager object in each scene

--- Assets/Scripts/Game Managers/GameManagerController.cs
+++ Assets/Scripts/Game Managers/GameManagerController.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class GameManagerController : MonoBehaviour {
 
+    public static GameManagerController Instance;
+
+    void Awake()
+    {
+        if(Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        } else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
 	// Use this for initialization
 	void Start () {
-		DontDestroyOnLoad(gameObject);
+		
 	}
 	
 	// Update is called once per frame
