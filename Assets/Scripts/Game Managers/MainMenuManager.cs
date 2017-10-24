a406c3427530107f7eaecca0a824253e8fea7105
Leonard Andrew Spencer <thrawn369@gmail.com>
2017-10-24 11:24:38 +1030

    MainMenu has settings, credits, how to play screens

--- Assets/Scripts/Game Managers/MainMenuManager.cs
+++ Assets/Scripts/Game Managers/MainMenuManager.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class MainMenuManager : MenuManager {
 
+    //TODO consider changing these to canvases?
+    public GameObject MenuOptions;
+    public GameObject Settings;
+    public GameObject HowToPlay;
+    public GameObject Credits;
+
+    private GameObject CurrentScreen;
+
 	// Use this for initialization
 	void Start () {
-		
+        Settings.SetActive(false);
+        HowToPlay.SetActive(false);
+        Credits.SetActive(false);
+        ChangeScreen(MenuOptions);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void ChangeScreen(GameObject newScreen)
+    {
+        if(CurrentScreen != null)
+        {
+            CurrentScreen.SetActive(false);
+        }
+        newScreen.SetActive(true);
+        CurrentScreen = newScreen;
+    }
 }
