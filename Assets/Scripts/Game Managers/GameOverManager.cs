03f249242867c246cf55b86624df6c5baeb52ffb
Leonard Andrew Spencer <thrawn369@gmail.com>
2017-10-30 14:59:04 +1030

    GameOver scene now shows how many coins were collected

--- Assets/Scripts/Game Managers/GameOverManager.cs
+++ Assets/Scripts/Game Managers/GameOverManager.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOverManager : MenuManager {
 
+    public Text TreasureCollectedText;
+
+
 	// Use this for initialization
 	void Start () {
-		
+        float treasureCollected = GameManagerController.Instance.CoinsCollected;
+        float totalTreasure = GameManagerController.Instance.TotalCoins;
+
+        TreasureCollectedText.text = "Treasure Collected:\n" + treasureCollected.ToString() + "/" + totalTreasure.ToString();
 	}
 	
 	// Update is called once per frame
