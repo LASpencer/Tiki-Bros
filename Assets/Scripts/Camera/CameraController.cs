61dde5a0d4f6615cf4cc794f40bb190abc70d15d
Leonard Andrew Spencer <thrawn369@gmail.com>
2017-10-30 11:14:53 +1030

    Camera can now zoom in and out with scrollwheel

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -6,17 +6,23 @@ public class CameraController : MonoBehaviour
 {
     public Transform target;
 
-    public Vector3 offset;
+    public float offset;
 
     public bool useOffsetValues;
 
     public float rotateSpeed;
+
+    public float zoomSpeed;
 
     public Transform pivot;
 
     public float maxViewAngle;
 
     public float minViewAngle;
+
+    public float maxDistance;
+
+    public float minDistance;
 
     public bool invertY;
 
@@ -25,10 +31,10 @@ public class CameraController : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-        if (!useOffsetValues)
-        {
-            offset = target.position - transform.position;
-        }
+        //if (!useOffsetValues)
+        //{
+        //    offset = target.position - transform.position;
+        //}
 
         pivot.transform.position = target.transform.position;
         pivot.transform.parent = target.transform;
@@ -50,39 +56,49 @@ public class CameraController : MonoBehaviour
             // Get the Y position of mouse and rotate pivot.
             float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
             //pivot.Rotate(-vertical, 0, 0);
+
+            float zoom = -Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+
             if (invertY)
             {
-                pivot.Rotate(vertical, 0, 0);
-            }
-            else
-            {
-                pivot.Rotate(-vertical, 0, 0);
+                vertical = -vertical;
             }
 
-            // Limit up and down camera rotation
-            if (pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180f)
+            // Limit pitch to within rotation
+            float pivotPitch = pivot.rotation.eulerAngles.x;
+            float newPitch = pivotPitch + vertical;
+            
+            if(newPitch > 180f)
             {
-                pivot.rotation = Quaternion.Euler(maxViewAngle, 0, 0);
-            }
-            if (pivot.rotation.eulerAngles.x > 180f && pivot.rotation.eulerAngles.x < 360f + minViewAngle)
-            {
-                pivot.rotation = Quaternion.Euler(360f + minViewAngle, 0, 0);
+                newPitch -= 360f;
             }
 
+                if (newPitch > maxViewAngle)
+                {
+                    vertical += (maxViewAngle - newPitch);
+                } 
+                if (newPitch < minViewAngle)
+                {
+                    vertical += (minViewAngle - newPitch);
+                }
+
+            pivot.Rotate(vertical, 0, 0);
+
+            offset = Mathf.Clamp(offset + zoom, minDistance, maxDistance);
 
             // move the camera based on current rotation of target, using original offset
             float desiredYAngle = target.eulerAngles.y;
             float desiredXAngle = pivot.eulerAngles.x;
 
             Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
-            transform.position = target.position - (rotation * offset);
+            transform.position = target.position - (pivot.forward * offset);
 
             //  transform.position = target.position - offset;
 
-            if (transform.position.y < target.position.y)
-            {
-                transform.position = new Vector3(transform.position.x, target.position.y, transform.position.z);
-            }
+            //if (transform.position.y < target.position.y)
+            //{
+            //    transform.position = new Vector3(transform.position.x, target.position.y, transform.position.z);
+            //}
             transform.LookAt(target);
         }
     }
