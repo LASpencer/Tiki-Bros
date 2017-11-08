20288d3d4df1715fc456ca4d02cdad8b6704b174
LASpencer <thrawn369@gmail.com>
2017-11-08 11:57:17 +1030

    Animation (#18)
    * Added Rigged models, Animations and animation controller. Remade the player controller Prefab to work correctly with these animations. awaiting playerstate.cs to be updated until animations work correctly.
    
    * Added animation controller to enemies, added scorpion prefab
    
    * Added Scorpion AI stuff, stopped player from sliding
    
    * Add colliders to pillars
    
    * Added "Anim" folder, containing updated characters. Clean up once animations are working.
    
    * Added "anim" folder, CLEAN UP WHEN WORKING
    
    * Changing animation controls
    
    * Put animation parameters into script
    
    * Fixed character stuck in JumpStart animation
    
    * Renamed Tiki prefab to Tiki_Character
    
    * no message
    
    * Added Punching state to player
    
    * Punching state activates hitbox trigger in front of player
    
    * Punching has windup before punch activates

---
+++ Assets/Scripts/Character/HitboxController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitboxController : MonoBehaviour {
+
+    public PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        //TODO if punching an enemy, enemy killed
+        //TODO give enemies rigidbody/character controller so they can be punched
+    }
+}
