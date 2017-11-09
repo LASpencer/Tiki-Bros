256a83accb5b4b49feb2a000652ff59c0d7c9b88
phillip <p.pajor@hotmail.com.au>
2017-11-09 20:48:00 +1030

    I have tried to get the footsteps to work, however I need Andrew to take a look as it should 100% be working.

---
+++ Assets/Scripts/Character/Footsteps.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Footsteps : MonoBehaviour
+{
+
+    public AudioClip[] clips;
+    private AudioSource a;
+    private bool once = false;
+    private const float timerReset = 0.18f;
+
+    void Awake()
+    {
+        a = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        string currTag = col.tag;
+        int currVal = -1;
+        if (currTag.Length <= 2)
+        {
+            currVal = Int32.Parse(currTag);
+        }
+        if (currVal != -1 && !once)
+        {
+            a.PlayOneShot(clips[currVal]);
+            once = true;
+            Invoke ("Reset", timerReset);
+        }
+    }
+    private void Reset()
+    {
+        once = false;
+    }
+}
