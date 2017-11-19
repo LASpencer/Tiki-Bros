01886c6fabf8e6ba10679fccbb6a440a7a711140
GeorgiaBryant <32627182+GeorgiaBryant@users.noreply.github.com>
2017-11-20 10:18:46 +1030

    Level refinement 2 - Checkpoint Updates (#22)
    * Added assets and textures
    
    * Added a different flower texture
    
    * Asset refinement - adding flowers and palmtrees
    
    * Player respawns at spawnpoint given by checkpoint
    
    * Flower placement
    
    * Added trees and flowers
    
    * Fleshing out level with collectables and platforms
    
    * Fixing enemy waypoints
    
    * Adding in platforms to final area
    
    * Added treasure to make path more obvious
    
    * Level now turns off cursor lock on ending.
    
    Added tooltips/renamed some fields
    
    * Finished adding treasure and palm trees (skinny palm)
    
    * Additional sprite updates
    
    * Gave waypoints to last crab. Rock platforms and tutorial stones now treated as static
    
    * Menu Updates and updated textures
    
    * UI updates with the menus, as well as invisible wall additions.
    
    * Checkpoint Activate and Deactivate functions declared
    
    * Checkpoint Updates
    
    * Updated Menu to include AIE logo

--- Assets/Scripts/Checkpoint/Checkpoint.cs
+++ Assets/Scripts/Checkpoint/Checkpoint.cs
@@ -11,6 +11,8 @@ public class Checkpoint : MonoBehaviour
     [Tooltip("Location at which player will respawn")]
     public GameObject spawnPoint;
 
+	public GameObject CheckpointParticles;
+
     // Use this for initialization
     void Start()
     {
@@ -28,8 +30,28 @@ public class Checkpoint : MonoBehaviour
     {
         if (other.CompareTag("Player") && levelManager.currentCheckpoint != this)
         {
+            if(levelManager.currentCheckpoint != null)
+            {
+                levelManager.currentCheckpoint.Deactivate();
+            }
             levelManager.currentCheckpoint = this;
+            this.Activate();
             gameObject.GetComponent<AudioSource>().PlayOneShot(ActivateSound);
         }
+    }
+
+    // Called when player reaches and activates checkpoint
+    public void Activate()
+    {
+        gameObject.GetComponent<AudioSource>().PlayOneShot(ActivateSound);
+        if (CheckpointParticles != null)
+            CheckpointParticles.SetActive(true);
+    }
+
+    // Called when another checkpoint is activated
+    public void Deactivate()
+    {
+        if (CheckpointParticles != null)
+            CheckpointParticles.SetActive(false);
     }
 }
---
+++ Assets/Scripts/SimpleRotateScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotateScript : MonoBehaviour {
+
+	public float rotateSpeed = 50f;
+
+	void Update()
+		{
+			transform.Rotate(0, rotateSpeed * Time.deltaTime,  0);
+		}
+}
