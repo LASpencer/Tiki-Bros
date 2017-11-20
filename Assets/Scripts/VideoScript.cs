05adfe9d2fb4da35ddb0417984f5b9799595cdc4
AIE\GeorgiaBryant <gsbryant@outlook.com>
2017-11-20 16:58:33 +1030

    Added Cutscene (and it works!)

---
+++ Assets/Scripts/VideoScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(AudioSource))]
+
+public class VideoScript : MonoBehaviour {
+
+	public MovieTexture CutSceneMaterial;
+
+	private MeshRenderer meshRenderer;
+
+	void Start(){
+		meshRenderer = GetComponent<MeshRenderer>();
+		meshRenderer.material.mainTexture = CutSceneMaterial;
+
+		AudioSource audio = GetComponent<AudioSource>();
+		audio.Play();
+
+		CutSceneMaterial.Play ();
+	}
+
+	void OnMouseDown()
+	{
+		CutSceneMaterial.Stop ();
+		//Application.LoadLevel("S1_Tutorial");
+		GameManagerController.Instance.LoadScene("S1_Tutorial"); 
+	}
+
+	void Update()
+	{
+		if (!CutSceneMaterial.isPlaying) GameManagerController.Instance.LoadScene("S1_Tutorial");
+	}
+}
