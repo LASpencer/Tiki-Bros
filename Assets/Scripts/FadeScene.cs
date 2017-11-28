4b1ee9172fd06197835960f772e6de826f21eb54
AIE\GeorgiaBryant <gsbryant@outlook.com>
2017-11-28 11:48:40 +1030

    Level refinements in Audio

---
+++ Assets/Scripts/FadeScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeScene : MonoBehaviour {
+
+	Renderer renderer;
+	public float alpha = 1.0f;
+
+	IEnumerator Wait()
+	{
+		yield return new WaitForSeconds (2f);
+	}
+
+	public void Update ();
+	{
+
+	}
+
+}
--- Assets/Scripts/TikiHand_Collectable.cs
+++ Assets/Scripts/TikiHand_Collectable.cs
@@ -3,29 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 //TODO maybe this should inherit from collectable?
 public class TikiHand_Collectable : MonoBehaviour {
 
-
     public float rotateSpeed = 35f;
+	//public GameObject fadeImage;
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player")){
-            Destroy(gameObject);
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player")){
+			//fadeImage.SetActive(true);
+			Destroy(gameObject);
 			GameManagerController gameManager = GameManagerController.Instance;
 			if (gameManager.CoinsCollected == gameManager.TotalCoins) {
 				GameManagerController.Instance.LoadScene("WinScene");
 			} else {
 				GameManagerController.Instance.LoadScene("LoseScene");
 			}    
-            
-        }
 
-    }
+		}
 
-    void Update()
-    {
-        transform.Rotate(0, rotateSpeed * Time.deltaTime,  0);
-    }
+	}
+		
+
+	void Update()
+	{
+		transform.Rotate(0, rotateSpeed * Time.deltaTime,  0);
+	}
 }
